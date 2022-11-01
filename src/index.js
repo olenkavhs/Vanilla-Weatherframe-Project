72fa15cb1cc66d9155c0258de5060b81a5180e6c
olenkavhs <111802610+olenkavhs@users.noreply.github.com>
2022-11-01 23:51:24 +0200

    Fixed days in forecast for next day

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `
